fa213f2dba56665caca8bed6fdb0cb6a820e83ee
quanle1872 <quanle1872@gmail.com>
2021-05-31 22:16:27 -0700

    AutoSave

--- src/pages/editor/document-editor/index.js
+++ src/pages/editor/document-editor/index.js
@@ -4,30 +4,29 @@ import {useCallback, useEffect, useState} from "react";
 import {database} from "../../../firebase";
 // import {InlineMath} from "react-katex";
 import {Render} from "../rendering";
-import FlashMessage from 'react-flash-message';
 import './style.css'
 
-export const DocumentEditor = ( {docRef, onSave}) => {
+export const DocumentEditor = ( {docRef}) => {
 
     const [text, setText] = useState('');
-    const [dirtyFlag, setDirtyFlag] = useState(false)
+    let preText = "";
+    let posText = text;
+
     const onTextChanged = useCallback(async (value, delta, source, editor) => {
         // console.log(`==> Text: ${text}`);
         const content = editor.getText();
             if (content.endsWith(' /\n')) {
               alert('about to load function component');
             }
-        // const docId = docRef.id;
-        // const doc = await database.document.doc(docId);
-        // await doc.update({body: text});
+
         setText(value);
     }, []);
 
+    // read document body
     useEffect(() => {
       if (docRef === null || docRef === undefined ) {
         return;
       }
-
       const doc = database.document.doc(docRef.id);
       const promise = doc.get();
       promise.then((result) => {
@@ -38,37 +37,35 @@ export const DocumentEditor = ( {docRef, onSave}) => {
 
       
     }, [docRef]);
-    const Saving = useCallback (async () => {
+    // Autosave function
+    async function Saving () {
+      preText = text
       const docId = docRef.id;
       const doc = await database.document.doc(docId);
-      const t = await doc.update({body: text});
+      doc.update({body: text});
+      console.log("Saved")
       
-    }, []);
-    // setInterval(Saving(), { alert("Hello"); }, 3000);
+    };
+    
     useEffect(() => {
       const interval = setInterval(() => {
-        console.log('This will run every second!');
-      }, 1000);
+        if ( posText != preText )  {
+          Saving();
+        }
+        else {
+          return
+        }
+      },3000);
+      
       return () => clearInterval(interval);
-    }, []);
-
-
-    // const onSaveCallback = useCallback(async () => {
-    //     const docId = docRef.id;
-    //     const doc = await database.document.doc(docId);
-    //     const t = await doc.update({body: text});
-    //     onSave();
-    // }, [docRef, onSave, text]);
-    // console.log("onsave",text)
+    }, );
     
-    
+
     return (
     <>
       <div className="text-area">
-        {/* {docRef && `${docRef.name}` } */}
         <ReactQuill value={text} onChange={onTextChanged} />
 
-        {/* <Button onClick={onSaveCallback}>Save</Button> */}
       </div>
       <div className='render-area'>
         <Render input = {text} />
