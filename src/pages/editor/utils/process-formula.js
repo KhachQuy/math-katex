1e01df2a4f226812c4130ee7370b989039bfc7c0
quanle1872 <quanle1872@gmail.com>
2021-10-06 12:45:59 -0700

    Update process-formula.js

--- src/pages/editor/utils/process-formula.js
+++ src/pages/editor/utils/process-formula.js
@@ -1,3 +1,26 @@
+import { Group } from "@material-ui/icons";
+
 export const processFormula = (formula) => {
-  return formula;
+  //  (y / 2)/(x^2)  === > \\frac{\\frac{y}{2}}{x^2}
+  
+  formula = formula.replace(' ', '')
+  const words = formula.split('')
+
+  function find (a_str, signal){
+    const found = ((element) => {
+      return element == signal;
+    })
+    return a_str.findIndex(found)
+  };
+
+  // (...
+  const open = find(words, '(');
+  const close = find(words.splice(open+1), ')')
+
+  const group = words.slice(open,close)
+  console.log(group)
+
+
+  return formula
 };
+//  quan le 
